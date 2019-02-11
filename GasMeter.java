952f115f711d51d37685ef26d6bfbb177f3c2cb6
TarasKunanets <44076970+TarasKunanets@users.noreply.github.com>
2019-02-11 11:05:41 +0200

    Fixed Lab1(Java) GasMeter

--- GasMeter.java
+++ GasMeter.java
@@ -1,177 +1,152 @@
 
 import java.awt.TextArea;
 import java.util.Scanner;
+
 public class GasMeter {
-	public int Qmax;
-	public int Distance;
-	public String Name;
-	private String Factory;
-	private String Type;
-	protected String Customer;
-	protected int SerialNumber;
-	static int Number=0;
-	
-	
-		void SetQmax (int _Qmax) 
-		{
-		Qmax= _Qmax;
-		}
-		
-		void SetDistance(int _Distance)
-		{
-		Distance = _Distance;
-		}
-	
-		void SetName(String _Name)
-		{
-		Name = _Name;
-		}
-		
-		void SetFactory(String _Factory)
-		{
-		Factory= _Factory;
-		}
-		
-		void SetType(String _Type)
-		{
-		Type = _Type;
-		}
-		
-		void SetCustomer(String _Customer) 
-		{
-		Customer=_Customer;
-		}
-		
-		void SetSerialNumber (int _SerialNumber) 
-		{
-	   SerialNumber= _SerialNumber;	
-		}
-	
-			int GetQmax () 
-			{
-				return Qmax;
-	  		}
-	
-			int GetDistance() 
-			{
-				return Distance;
-			}
-	
-			String GetName()
-			{
-				return Name;
-			}
-			
-			String GetFactory() 
-			{
-				return Factory;
-			}
-			
-			String GetType() 
-			{
-				return Type;
-			}
-			
-			String GetCustomer()
-			{
-				return Customer;
-			}
-			
-			int GetSerialNumber() 
-			{
-				return SerialNumber;
-			}
-			
-		    static int GetNumber() 
-		    {
-		    	return Number;
-		    }
-   
-   public GasMeter() {
-	Number++;
-} 
-	public  GasMeter(int Qmax, int Distance, String Name,String Factory)
-	{
-		this.SetQmax(Qmax);
-		this.SetDistance(Distance);
-		this.SetName(Name);
-		this.SetFactory(Factory);
+	public int qmax;
+	public int distance;
+	public String name;
+	private String factory;
+	private String type;
+	protected String customer;
+	protected int serialNumber;
+	static int number = 0;
+
+	void setQmax(int qmax) {
+		this.qmax = qmax;
+	}
+
+	void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+	void setFactory(String factory) {
+		this.factory = factory;
+	}
+
+	void setType(String type) {
+		this.type = type;
+	}
+
+	void setCustomer(String customer) {
+		this.customer = customer;
+	}
+
+	void setSerialNumber(int serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	int getQmax() {
+		return qmax;
+	}
+
+	int getDistance() {
+		return distance;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	String getFactory() {
+		return factory;
+	}
+
+	String getType() {
+		return type;
+	}
+
+	String getCustomer() {
+		return customer;
+	}
+
+	int getSerialNumber() {
+		return serialNumber;
+	}
+
+	static int getNumber() {
+		return number;
+	}
+
+	public int Number() {
+		number++;
+		return number;
+	}
+	public GasMeter(){
 		
 	}
-	
-	public GasMeter(int Qmax, int Distance, String Name,String Factory ,String Type ,String Customer ,int SerialNumber  )
-	
-	{
-		
-		SetQmax(Qmax);
-		SetDistance(Distance);
-		SetName(Name);
-		SetFactory(Factory);
-		SetType(Type);
-		SetCustomer(Customer);
-		SetSerialNumber(SerialNumber);
-		
+
+	public GasMeter(int qmax, int distance, String name, String factory) {
+		this.qmax = qmax ;
+		this.distance = distance;
+		this.name = name ;
+		this.factory = factory;
+
 	}
-	
-	public GasMeter(int Qmax, int Distance, String Name)
+
+	public GasMeter(int qmax, int distance, String name, String factory, String type, String customer, int serialNumber)
+
 	{
-		SetQmax(Qmax);
-		SetDistance(Distance);
-		SetName(Name);
+
+		this(qmax, distance , name , factory);
+		this.type=type;
+		this.customer=customer;
+		this.serialNumber=serialNumber;
+
 	}
-	
-	 @Override
+
+
+	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		 System.out.println("Qmax = " + GetQmax());
-		 System.out.println("Distance = " + GetDistance());
-			System.out.println("Name = " + GetName() + "\n");
+		System.out.println("Qmax = " + getQmax());
+		System.out.println("Distance = " + getDistance());
+		System.out.println("Name = " + getName() + "\n");
 		return super.toString();
-		
+
 	}
+
 	static void printStaticNumber() {
-		 System.our.println(Number);
-	 }
- 
-		// TODO Auto-generated method stub
+		System.out.println(number);
+	}
 
 	
-	
-	
-		
+
 	public static void main(String[] args) {
-		
-	GasMeter A = new GasMeter(4, 5, "Q");
-	System.out.println("Qmax = " + A.toString());
-	
-		
-	System.out.println("Qmax = " + A.GetQmax());
-	System.out.println("Distance = " + A.GetDistance());
-	System.out.println("Name = " + A.GetName() + "\n");
-	
-	GasMeter B = new GasMeter(1, 3, "W");
-	System.out.println("Qmax = " + B.toString());
-	System.out.println("Qmax = " + B.GetQmax());
-	System.out.println("Distance = " + B.GetDistance());
-	System.out.println("Name = " + B.GetName() + "\n");
-	
-	GasMeter S = new GasMeter(222, 33, "E");
-	
-	System.out.println("Qmax = " + S.GetQmax());
-	System.out.println("Distance = " + S.GetDistance());
-	System.out.println("Name = " + S.GetName() + "\n");
-	
-	GasMeter D = new GasMeter(11, 55, "R");
-	
-	System.out.println("Qmax = " + D.GetQmax());
-	System.out.println("Distance = " + D.GetDistance());
-	System.out.println("Name = " + D.GetName() + "\n");
-	
-	GasMeter F = new GasMeter(66, 7, "T");
-	
-	System.out.println("Qmax = " + F.GetQmax());
-	System.out.println("Distance = " + F.GetDistance());
-	System.out.println("Name = " + F.GetName() + "\n");
+
+		GasMeter MetriG1 = new GasMeter(4, 5, "Q","GsFacrory");
+		System.out.println("Qmax = " + MetriG1.toString());
+
+		System.out.println("Qmax = " + MetriG1.getQmax());
+		System.out.println("Distance = " + MetriG1.getDistance());
+		System.out.println("Name = " + MetriG1.getName() + "\n");
+
+		GasMeter Elster = new GasMeter(1, 3, "W" ,"LvivFacrory");
+		System.out.println("Qmax = " + Elster.toString());
+		System.out.println("Qmax = " + Elster.getQmax());
+		System.out.println("Distance = " + Elster.getDistance());
+		System.out.println("Name = " + Elster.getName() + "\n");
+
+		GasMeter Vizar = new GasMeter(222, 33, "E" ,"MetrixFacrory");
+
+		System.out.println("Qmax = " + Vizar.getQmax());
+		System.out.println("Distance = " + Vizar.getDistance());
+		System.out.println("Name = " + Vizar.getName() + "\n");
+
+		GasMeter SamGas = new GasMeter(11, 55, "R" ,"VizarFacrory");
+
+		System.out.println("Qmax = " + SamGas.getQmax());
+		System.out.println("Distance = " + SamGas.getDistance());
+		System.out.println("Name = " + SamGas.getName() + "\n");
+
+		GasMeter MetrixUG = new GasMeter(66, 7, "T" ,"ElsterFacrory");
+
+		System.out.println("Qmax = " + MetrixUG.getQmax());
+		System.out.println("Distance = " + MetrixUG.getDistance());
+		System.out.println("Name = " + MetrixUG.getName() + "\n");
 	}
 
 }
